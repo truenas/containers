197a1f82681a05f21cfb04bdc88fb1f01b07043d
Stavros Kois <s.kois@outlook.com>
2025-02-25 21:11:37 +0200

    fix typo

--- apps/nextcloud-fpm/configure-scripts/occ-imaginary.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-imaginary.sh
@@ -2,7 +2,7 @@
 
 occ_imaginary_install() {
   echo '## Configuring Imaginary...'
-  occ config:system:set preview_imaginary_url --value="${IX_IMAGINARY_URL:?"NX_IMAGINARY_URL is unset"}"
+  occ config:system:set preview_imaginary_url --value="${IX_IMAGINARY_URL:?"IX_IMAGINARY_URL is unset"}"
 }
 
 occ_imaginary_remove() {
