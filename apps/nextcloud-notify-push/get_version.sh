e35d4efed23a9b026f81f4c8d97dcba715843dda
Stavros Kois <s.kois@outlook.com>
2025-02-27 20:39:13 +0200

    fix get version

--- apps/nextcloud-notify-push/get_version.sh
+++ apps/nextcloud-notify-push/get_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curr_dir="$1"
 
-VERSION=$(grep "ENV NOTIFY_PUSH_VERSION" "$curr_dir"/Dockerfile | cut -d ' ' -f3)
+VERSION=$(grep "ENV NOTIFY_PUSH_VERSION" "$curr_dir"/Dockerfile | cut -d '=' -f2)
 
 # Generate a build id based on the files in the current directory
 build_id=$(find "$curr_dir" -type f -exec sha256sum {} \; | sort | sha256sum | cut -d ' ' -f1)
