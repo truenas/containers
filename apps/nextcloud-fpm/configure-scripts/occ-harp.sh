be354f57be16f95a9338f558b4995ddd8a490a3d
Stavros Kois <s.kois@outlook.com>
2026-04-09 18:56:42 +0300

    fix

--- apps/nextcloud-fpm/configure-scripts/occ-harp.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-harp.sh
@@ -14,8 +14,8 @@ occ_harp_install() {
     occ app_api:daemon:unregister tn-harp
   fi
 
-  occ app_api:daemon:register -vvv tn-harp "HaRP (TrueNAS)" "docker-install" "http" \
-    "${IX_HARP_INTERNAL_URL:?"IX_HARP_INTERNAL_URL is unset"}" "${IX_HARP_NC_INTERNAL_URL:?"IX_HARP_NC_INTERNAL_URL is unset"}" \
+  occ app_api:daemon:register tn-harp "HaRP (TrueNAS)" "docker-install" "http" \
+    "${IX_HARP_INTERNAL_ADDRESS:?"IX_HARP_INTERNAL_ADDRESS is unset"}" "${IX_HARP_NC_INTERNAL_URL:?"IX_HARP_NC_INTERNAL_URL is unset"}" \
     --net="${IX_HARP_NETWORK:?"IX_HARP_NETWORK is unset"}" \
     --harp_frp_address="${IX_HARP_INTERNAL_FRP_ADDRESS:?"IX_HARP_INTERNAL_FRP_ADDRESS is unset"}" \
     --harp_shared_key="${IX_HARP_SHARED_KEY:?"IX_HARP_SHARED_KEY is unset"}" \
